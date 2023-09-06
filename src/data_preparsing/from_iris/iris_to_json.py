b9409ac30ae86a1458e2544852a5225bde3d97e6
Luca Visentin <luca.visentin@unito.it>
2023-09-06 11:59:34 +0200

    feat: Added prototype iris data to json converter

--- src/data_preparsing/from_iris/iris_to_json.py
+++ src/data_preparsing/from_iris/iris_to_json.py
@@ -6,16 +6,27 @@ from dataclasses import dataclass
 from typing import Optional
 from uuid import uuid4
 from jellyfish import jaro_winkler_similarity as jws
-from io import StringIO
+from io import StringIO, IOBase
+from tqdm import tqdm
+import csv
+import logging
+import json
+import numpy as np
+import sys
 
+logging.basicConfig(level=logging.INFO)
+
+log = logging.getLogger(__name__)
 
 class Author:
     def __init__(self, name: str, surname: str, affiliation: str, department: Optional[str] = None, id: Optional[str] = None) -> None:
+        log.debug(f"Creating author {name} {surname} {affiliation} {department} {id}")
         # TODO: perhaps we could remove weird unicode characters? To make the matching easier?
-        self.name = name.lower().strip()
-        self.surname = surname.lower().strip()
-        self.affiliation = affiliation.lower().strip()
-        self.department = department.lower().strip() if department is not None else None
+        # The guarding IFs are to get around NAs
+        self.name = name.lower().strip() if name and pd.notna(name) else None
+        self.surname = surname.lower().strip() if surname and pd.notna(surname) else None
+        self.affiliation = affiliation.lower().strip() if affiliation and pd.notna(affiliation) else None
+        self.department = department.lower().strip() if department and pd.notna(department) else None
         self.id = id or str(uuid4())
 
         assert self.id is not None, "ID should not be None"
@@ -36,10 +47,10 @@ class Author:
         # This is a weighted average, were we give more importance to the name and surname
         # The rationale is that the affiliation and department are more likely to change
         # between roles
-        return (jws(self.name, other.name) + \
-            jws(self.surname, other.surname) + \
-            jws(self.affiliation, other.affiliation) * 0.5 + \
-            jws(self.department, other.department) * 0.5 ) / 3
+        return (jws(self.name or "", other.name or "") + \
+            jws(self.surname or "", other.surname or "") + \
+            jws(self.affiliation or "", other.affiliation or "") * 0.5 + \
+            jws(self.department or "", other.department or "") * 0.5 ) / 3
 
 
 class AuthorGlobber:
@@ -72,34 +83,67 @@ class AuthorGlobber:
 
 @dataclass
 class Paper:
-    id: int
+    id: str
     title: int
     year: int
     authors: list[str]
 
 
-def parse_file(gobbler: AuthorGlobber, data: pd.DataFrame) -> list[Paper]:
-    # For every paper, we create a list of authors, then check if we have seen them before
-    # If we have, we use the same ID, otherwise we create a new author, and add it to the gobbler
+def parse_file_simple(gobbler: AuthorGlobber, data: pd.DataFrame) -> list[Paper]:
+    # Here, we do not do any fancy matching, we just add the authors if they
+    # are "recognized"
+    # The resulting network will only have known authors. Not sure if this is ok
+    # but it's a start.
 
     papers = []
 
-    for paper_id, paper in data.groupby("paper_id"):
-        pass
+    for paper_id, paper in tqdm(data.groupby("handle")):
+
+        # Extract the authors and add them to the gobbler
+        authors = []
+        for _, row in paper.iterrows():
+            author = Author(
+                name=row["author_name"],
+                surname=row["author_surname"],
+                affiliation="University of Turin (maybe)",
+                department=row["author_department"],
+                id=row["author_cris_id"],
+            )
+            result = gobbler.add_or_glob(author)
+            authors.append(result)
+
+        paper_obj = Paper(
+            id = paper_id,
+            title = paper["title"].iloc[0], # The titles should all be the same
+            # TODO: We should probably check that the titles are the same
+            year = paper["year"].iloc[0], # The years should all be the same
+            # TODO: We should probably check that the years are the same,
+            authors=authors,
+        )
+        papers.append(paper_obj)
+    
+    log.info(f"Found {len(papers)} papers")
+    log.debug(papers[0])
+
+    return papers
 
 
 def read_iris_data(path: Path) -> pd.DataFrame:
     """Read the iris data from the given path"""
+    log.info(f"Reading {path} to an IRIS dataset")
     # Edit the header
     data = StringIO()
     with path.open("r") as stream:
-        header = stream.readline().split(",")
+        reader = csv.reader(stream)
+        header = next(reader)
         header = [standardize_header(head.strip('"')) for head in header]
 
         data.write(",".join(header) + "\n")
 
         for line in stream:
             data.write(line)
+
+    data.seek(0)
 
     data = pd.read_csv(
         data,
@@ -132,6 +176,7 @@ def standardize_header(head: str) -> str:
         "Rivista/Serie: ISSN": "journal_issn",
         "Rivista: codice ANCE": "journal_ance_code",
         "rivista: DOAJ (si/no)": "journal_is_doaj",
+        "rivista: policy sherpa/romeo per pre-print": "journal_sherpa_romeo_preprint_policy",
         "rivista: policy sherpa/romeo per versione editoriale": "journal_sherpa_romeo_policy",
         "rivista: policy sherpa/romeo per post-print": "journal_sherpa_romeo_postprint_policy",
         "rivista: editore": "journal_publisher",
@@ -151,11 +196,41 @@ def standardize_header(head: str) -> str:
         return matches[head]
     
     raise ValueError(f"Unknown header: {head}")
+
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        if isinstance(obj, np.floating):
+            return float(obj)
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return super(NpEncoder, self).default(obj)
+
+
+def main(folder: Path, output_steam: IOBase):
+    files = list(folder.glob("*.csv"))
+    log.info(f"Found {len(files)} files. Reading them in...")
+    datasets = [read_iris_data(path) for path in files]
+
+    author_gobbler = AuthorGlobber()
+    papers = []
+    for dataset in datasets:
+        papers.extend(parse_file_simple(author_gobbler, dataset))
     
+    log.info(f"Found {len(author_gobbler.authors)} authors")
+    log.info(f"Found {len(papers)} papers")
 
-def main(folder: Path):
-    files = folder.glob("*.csv")
+    json.dump(
+        {
+            "authors": [x.__dict__ for x in author_gobbler.authors.values()],
+            "papers": [x.__dict__ for x in papers],
+        },
+        output_steam,
+        cls=NpEncoder,
+    )
 
+    return None
 
 
 if __name__ == "__main__":
@@ -164,7 +239,20 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("folder", help="Folder containing the iris data", type=Path)
+    parser.add_argument("--output_file", help="Output file", type=Path, default=None)
+    parser.add_argument("-v", "--verbose", action = "count", default = 0, help = "Increase verbosity")
 
     args = parser.parse_args()
 
-    main(args.folder)
+    verbosity_levels = {
+        0: logging.WARNING,
+        1: logging.INFO,
+        2: logging.DEBUG,
+    }
+
+    for i, level in verbosity_levels.items():
+        if args.verbose >= i:
+            log.setLevel(level)
+
+    output_stream = args.output_file.open("w+") if args.output_file else sys.stdout
+    main(args.folder, output_stream)
